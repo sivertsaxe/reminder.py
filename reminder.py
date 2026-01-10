0176989d4a6930b28c1d7f2eb2cf935626426ff4
sivertsaxe <sivertsaxe@gmail.com>
2026-01-11 00:13:34 +0100

    Create reminder.py

---
+++ reminder.py
@@ -0,0 +1,15 @@
+import os, requests
+
+TOKEN = os.environ["TG_TOKEN"]
+CHAT_ID = os.environ["TG_CHAT_ID"]
+
+text = "⏰ Erinnerung: Bitte vergiss nicht abzustimmen!"
+
+requests.post(
+    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+    json={
+        "chat_id": CHAT_ID,
+        "text": text
+    },
+    timeout=20
+)
